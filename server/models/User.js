6387043ecbf918cf4c2930684c6c00af0889265c
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-07-26 22:28:10 +0530

    cloudinary added

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     fullName: {type: String, required: true},
     password: {type: String, required: true, minlength: 6},
     profilePic: {type: String, default: ""},
+    address: {type: String},
 }, {timestamps: true});
 
 const User = mongoose.model("User", userSchema);
